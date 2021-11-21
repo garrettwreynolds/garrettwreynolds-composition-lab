74f2fbf9b6c0efcf95de17f17c8fbe4ec08aef1c
Garrett Reynolds <wgreynolds@crimson.ua.edu>
2021-11-21 17:53:55 -0600

    setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace garrettwreynolds_composition_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
